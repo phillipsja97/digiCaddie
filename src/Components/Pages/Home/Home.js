6f3c89a897e87ea1ea59701842dace8f81e8150b
Jamie <phillipsja97@gmail.com>
2020-01-21 18:30:58 -0600

    homeComponent

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import './Home.scss';
 
 class Home extends React.Component {
   render() {
+    var user = firebase.auth().currentUser;
+    const name = user.displayName;
+    const userEmail = user.email;
+    const photo = user.photoURL;
     return (
       <div className="Home">
       <h1>Hello</h1>
+      <h1>{name}</h1>
+      <h1>{userEmail}</h1>
+      <img src={photo} className="userPhoto"></img>
       </div>
     );
   }
